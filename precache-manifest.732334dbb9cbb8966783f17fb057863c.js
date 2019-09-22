0277b85d5f60bb49019dc717eb018603d6928380
antonynizh <antonynizh@users.noreply.github.com>
2019-09-22 21:01:51 +0300

    Updates

---
+++ precache-manifest.732334dbb9cbb8966783f17fb057863c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminiappfirst/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "ef2c4164faddf6444cf1",
+    "url": "/vkminiappfirst/static/js/runtime~main.e6d0037b.js"
+  },
+  {
+    "revision": "7bb841b09a4162d37291",
+    "url": "/vkminiappfirst/static/js/main.f823a775.chunk.js"
+  },
+  {
+    "revision": "f0c95e71cacf498354a4",
+    "url": "/vkminiappfirst/static/js/2.a7c65eef.chunk.js"
+  },
+  {
+    "revision": "7bb841b09a4162d37291",
+    "url": "/vkminiappfirst/static/css/main.02774a71.chunk.css"
+  },
+  {
+    "revision": "f0c95e71cacf498354a4",
+    "url": "/vkminiappfirst/static/css/2.ef0c48ef.chunk.css"
+  },
+  {
+    "revision": "010333bc89ec3acbaa8c0c1bafc18c05",
+    "url": "/vkminiappfirst/index.html"
+  }
+];
